7903b6f2c558e6ecc719de616601c3a81fb092d7
Ali <aliifarajj@hotmail.com>
2024-10-08 14:05:15 +0300

    initial files

---
+++ src/app/layout.js
@@ -0,0 +1,22 @@
+import localFont from "next/font/local";
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+export const metadata = {
+  title: "Fruity Store | Home Page",
+  description:
+    "Shop fresh and preserved fruits, vegetables, dates, and a variety of foodstuffs at Fruity Store. Quality products delivered with care.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable}`}>{children}</body>
+    </html>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0055B6",
+        secondary: "#FFD74B",
+        silver: "#EFEFEF",
+        darkGray: "#212121",
+        danger: "#FF6863",
+        success: "#7AFF6D"
+      },
+    },
+  },
+  plugins: [],
+};
