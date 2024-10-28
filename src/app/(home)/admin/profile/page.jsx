844e446be4c14b6823c28e4a93b155f6b2673a69
Ali <aliifarajj@hotmail.com>
2024-10-29 01:02:55 +0300

    admin profile responsive

--- src/app/(home)/admin/profile/page.jsx
+++ src/app/(home)/admin/profile/page.jsx
@@ -7,7 +7,7 @@ export default function ProfilePage() {
         <h2 className="mb-3 sm:mb-5 text-lg sm:text-2xl text-darkGray font-semibold">
           Profile
         </h2>
-        <div className="mx-auto w-[50%] p-3 sm:p-5 bg-primary rounded-div flex flex-col gap-1 sm:gap-3 items-center">
+        <div className="mx-auto w-full md:w-[50%] p-3 sm:p-5 bg-primary rounded-div flex flex-col gap-1 sm:gap-3 items-center">
           <div className="w-20 sm:w-28 h-20 sm:h-28">
             <Image
               src="/images/user2.png"
