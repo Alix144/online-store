057d41ea30fb9415544bc0ab2f3a8530258e554a
Ali <aliifarajj@hotmail.com>
2024-10-29 00:33:23 +0300

    admin orders listDiv responsive

--- src/app/(home)/admin/orders/page.jsx
+++ src/app/(home)/admin/orders/page.jsx
@@ -2,7 +2,7 @@ import ListDiv from "@/components/list-div-component/ListDiv";
 
 export default function OrdersPage() {
   return (
-    <main className="p-10 flex flex-col gap-5 sm:gap-10">
+    <main className="p-5 sm:p-10 flex flex-col gap-5 sm:gap-10">
         <section className="w-full">
           <h2 className="mb-3 sm:mb-5 text-lg sm:text-2xl text-darkGray font-semibold">
             Orders
--- src/components/list-div-component/ListDiv.jsx
+++ src/components/list-div-component/ListDiv.jsx
@@ -57,14 +57,14 @@ export default function ListDiv({ type }) {
               <h5 className="font-semibold text-sm sm:text-base">Status</h5>
             </div>
           ) : type === "adminOrderList" ? (
-            <div className="px-16 py-1 w-full bg-silver rounded-div flex justify-between">
-              <div className="flex gap-20">
-                <h5 className="w-24 max-w-24 font-semibold">Customer</h5>
-                <h5 className="w-24 max-w-24 font-semibold">Date</h5>
-                <h5 className="w-24 max-w-24 font-semibold">Price</h5>
-                <h5 className="w-24 max-w-24 font-semibold">Items</h5>
+            <div className="px-5 lg:px-16 py-1 w-full bg-silver rounded-div flex justify-between scrollbar-hide overflow-x-auto">
+              <div className="flex gap-5 sm:gap-10 md:gap-20">
+                <h5 className="w-16 lg:w-24 font-semibold text-sm sm:text-base">Customer</h5>
+                <h5 className="w-10 sm:w-14 lg:w-24 font-semibold text-sm sm:text-base">Date</h5>
+                <h5 className="w-10 sm:w-14 lg:w-24 font-semibold text-sm sm:text-base">Price</h5>
+                <h5 className="w-10 sm:w-14 lg:w-24 font-semibold text-sm sm:text-base">Items</h5>
               </div>
-              <h5 className="font-semibold">Status</h5>
+              <h5 className="w-10 sm:w-14 lg:w-24 font-semibold text-sm sm:text-base">Status</h5>
             </div>
           ) : type === "productsList" ? (
             <div className="px-16 py-1 w-full bg-silver rounded-div flex justify-between">
