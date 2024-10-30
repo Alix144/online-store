5b4feca7cdf00b6bda9be93eaa16a0197d0c83de
Ali <aliifarajj@hotmail.com>
2024-10-30 21:48:34 +0300

    product in cart style

--- src/app/(home)/cart/page.jsx
+++ src/app/(home)/cart/page.jsx
@@ -4,57 +4,125 @@ import Product from "@/components/Product";
 export default function ProductsPage() {
   return (
     <main className="flex flex-col gap-5 sm:gap-10">
-      {true ?
-      
-      <section className="py-10 sm:py-14 lg:py-16 xl:py-20 flex flex-col gap-5 sm:gap-10">
-        <h1 className="m-auto text-center font-bold text-xl sm:text-2xl lg:text-3xl">
-          Cart
-        </h1>
-        <div className="flex gap-5 justify-center sm:justify-normal items-start">
-          <div className="w-[60%] hidden sm:flex gap-5 flex-wrap">
-            <Product />
-            <Product />
-            <Product />
-          </div>
-          {/* order summary div */}
-          <div className="p-5 md:p-10 lg:w-[40%] bg-primary rounded-div flex flex-col justify-between">
-            {/* adderss & order summary */}
-            <div className="mb-14 sm:mb-20">
-              <div className="mb-3 sm:mb-7">
-                <p className="text-sm sm:text-base font-bold text-white">Your Address</p>
-                <p className="text-sm sm:text-base text-white">
-                  123 Fresh Market St., Green Valley, Kuwait City, Kuwait.
-                </p>
-              </div>
-              <div>
-                <p className="text-sm sm:text-base font-bold text-white">Order Summary</p>
-                <div className="flex flex-col gap-2">
-                  <div className="w-full flex justify-between">
-                    <div className="flex gap-3 sm:gap-5">
-                      <p className="text-sm sm:text-base text-white">2 KG</p>
+      {true ? (
+        <section className="py-10 sm:py-14 lg:py-16 xl:py-20 flex flex-col gap-5 sm:gap-10">
+          <h1 className="m-auto text-center font-bold text-xl sm:text-2xl lg:text-3xl">
+            Cart
+          </h1>
+          <div className="flex gap-5 justify-center sm:justify-normal items-start">
+            <div className="w-[60%] hidden sm:flex gap-5 flex-wrap">
+              <Product />
+              <Product />
+              <Product />
+            </div>
+            {/* order summary div */}
+            <div className="p-5 md:p-10 lg:w-[40%] bg-primary rounded-div flex flex-col justify-between">
+              {/* adderss & order summary */}
+              <div className="mb-14 sm:mb-20">
+                {false ? (
+                  <div className="mb-3 sm:mb-7">
+                    <p className="text-sm sm:text-base font-bold text-white">
+                      Your Address
+                    </p>
+                    <p className="text-sm sm:text-base text-white">
+                      123 Fresh Market St., Green Valley, Kuwait City, Kuwait.
+                    </p>
+                  </div>
+                ) : (
+                  <div className="mb-3 sm:mb-7 flex flex-col gap-5 justify-between text-darkGray text-center">
+                    <p className="text-sm sm:text-base font-bold text-white text-left">
+                      Your Address
+                    </p>
+                    <div className="flex flex-col">
+                      <div className="flex gap-2">
+                        <input
+                          type="text"
+                          placeholder="Area"
+                          className="mb-3 px-3 w-[50%] h-8 bg-white rounded-div border-darkGray border-[1px]"
+                        />
+                        <input
+                          type="text"
+                          placeholder="Block"
+                          className="mb-3 px-3 w-[50%] h-8 bg-white rounded-div border-darkGray border-[1px]"
+                        />
+                      </div>
+                      <input
+                        type="text"
+                        placeholder="Street"
+                        className="mb-3 px-3 w-full h-8 bg-white rounded-div border-darkGray border-[1px]"
+                      />
+                      <div className="flex gap-2">
+                        <input
+                          type="text"
+                          placeholder="Building No"
+                          className="mb-3 px-3 w-[50%] h-8 bg-white rounded-div border-darkGray border-[1px]"
+                        />
+                        <input
+                          type="text"
+                          placeholder="Avenue (optional)"
+                          className="mb-3 px-3 w-[50%] h-8 bg-white rounded-div border-darkGray border-[1px]"
+                        />
+                      </div>
+                      <div className="flex gap-2">
+                        <input
+                          type="text"
+                          placeholder="Apt. number"
+                          className="mb-3 px-3 w-[50%] h-8 bg-white rounded-div border-darkGray border-[1px]"
+                        />
+                        <input
+                          type="text"
+                          placeholder="Floor"
+                          className="mb-3 px-3 w-[50%] h-8 bg-white rounded-div border-darkGray border-[1px]"
+                        />
+                      </div>
+                      <textarea
+                        name=""
+                        id=""
+                        placeholder="Additional directions (optional)"
+                        className="mb-3 px-3 w-full bg-white rounded-div border-darkGray border-[1px]"
+                      ></textarea>
+                    </div>
+                  </div>
+                )}
+                <div>
+                  <p className="text-sm sm:text-base font-bold text-white">
+                    Order Summary
+                  </p>
+                  <div className="flex flex-col gap-2">
+                    <div className="w-full flex justify-between">
+                      <div className="flex gap-3 sm:gap-5">
+                        <p className="text-sm sm:text-base text-white">2 KG</p>
+                        <p className="text-sm sm:text-base text-white">
+                          Tomatoes
+                        </p>
+                      </div>
                       <p className="text-sm sm:text-base text-white">
-                        Tomatoes
+                        0.600 KWD
                       </p>
                     </div>
-                    <p className="text-sm sm:text-base text-white">0.600 KWD</p>
                   </div>
                 </div>
               </div>
-            </div>
-            {/* second part */}
-            <div>
-              <div className="mb-5 flex justify-between border-t-[1px] border-white">
-                <p className="text-sm sm:text-base font-bold text-white">Total</p>
-                <p className="text-sm sm:text-base font-bold text-white">0.400 KWD</p>
+              {/* second part */}
+              <div>
+                <div className="mb-5 flex justify-between border-t-[1px] border-white">
+                  <p className="text-sm sm:text-base font-bold text-white">
+                    Total
+                  </p>
+                  <p className="text-sm sm:text-base font-bold text-white">
+                    0.400 KWD
+                  </p>
+                </div>
+                <button className="w-full font-bold btn-style bg-secondary">
+                  Place Order
+                </button>
               </div>
-              <button className="w-full font-bold btn-style bg-secondary">Place Order</button>
             </div>
           </div>
-        </div>
-      </section>
-      :
-      <OrderSuccess/>
-    }
+        </section>
+      ) : (
+        <OrderSuccess />
+      )}
     </main>
   );
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,18 @@
+"use client"
 import Image from "next/image";
+import { useState } from "react";
 
-export default function Product({ product, isFavorite }) {
+export default function Product({ product, isFavorite, inCart }) {
+  const [amount, setAmount] = useState(1)
+
+  const reduceAmount = () => {
+    if(amount > 1){
+      setAmount(amount-1)
+    }else if(amount === 1 || amount === 0.75 || amount === 0.5){
+      setAmount(amount - 0.25)
+    }
+  }
+
   return (
     <div className="p-3 w-52 sm:w-60 min-w-52 sm:min-w-60 h-52 sm:h-60 rounded-div bg-white border-lightGray border-[1px] flex flex-col gap-3 cursor-pointer">
       <div className="w-full h-[65%] rounded-div bg-lightGray relative">
@@ -16,19 +28,65 @@ export default function Product({ product, isFavorite }) {
       <div className="px-3 h-[35%] w-full flex justify-between">
         <div className="flex flex-col justify-between">
           <h3 className="text-lg font-semibold">{product?.name}</h3>
-          <p className="text-sm sm:text-base">{product?.price} KWD / {product?.measurement}</p>
+          <p className="text-sm sm:text-base">
+            {product?.price} KWD / {product?.measurement}
+          </p>
         </div>
 
-        <div className="flex flex-col justify-between">
-          <Image
-            src={`/images/${isFavorite ? 'filled-heart.png' : 'empty-heart.png'}`}
-            alt={`${isFavorite ? 'Filled' : 'Empty'} heart`}
-            width="20"
-            height="20"
-            className="cursor-pointer"
-          />
-          <Image src="/images/cart.png" alt="Cart" width="20" height="20" className="cursor-pointer"/>
-        </div>
+        {true ? (
+          <div className="flex flex-col justify-between items-end">
+            <Image
+              src="/images/trash-can.png"
+              alt="Trash Can"
+              width={15}
+              height={15}
+            />
+            <div className="flex gap-1 items-center">
+              <div className="w-4 h-4" onClick={()=>reduceAmount()}>
+                <Image
+                  src="/images/minus.png"
+                  alt="Minus"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <input
+                type="number"
+                className="px-1 w-10 h-5 border border-darkGray rounded-[5px]"
+                value={amount}
+                onChange={(e)=>setAmount(e.target.value)}
+              />
+              <p className="text-sm sm:text-base">kg</p>
+              <div className="w-4 h-4" onClick={()=>setAmount(amount + 1)}>
+                <Image
+                  src="/images/plus.png"
+                  alt="Plus"
+                  width={100}
+                  height={100}
+                />
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="flex flex-col justify-between">
+            <Image
+              src={`/images/${
+                isFavorite ? "filled-heart.png" : "empty-heart.png"
+              }`}
+              alt={`${isFavorite ? "Filled" : "Empty"} heart`}
+              width="20"
+              height="20"
+              className="cursor-pointer"
+            />
+            <Image
+              src="/images/cart.png"
+              alt="Cart"
+              width="20"
+              height="20"
+              className="cursor-pointer"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
